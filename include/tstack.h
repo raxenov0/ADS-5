fff8c81a25eb3f0c42e4f491ecd899e052b91906
Аксенов Роман <98100948+raxenov0@users.noreply.github.com>
2022-03-24 23:04:30 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,38 +4,40 @@
 
 template<typename T, int size1>
 class TStack {
-public:
-  TStack() {
-    this->size = size1;
-    this->locSize = -1;
-    this->arr = new T[size];
-  };
-  ~TStack() {
-    delete []arr;
-  };
-  void push_back(T elem) {
-    if (locSize >= size - 1) {
-      T* tmp = arr;
-      arr = new T[++size];
-      for (int i = 0; i < size; i++) arr[i] = tmp[i];
-      delete[] tmp;
+  public:
+    TStack() {
+      this->size = size1;
+      this->locSize = -1;
+      this->arr = new T[size];
     }
-    arr[++locSize] = elem;
-  };
-  T& pop_back() {
-    size--;
-    return arr[--locSize + 1];
-  }
-  T& getLast() {
-    return arr[locSize];
-	}
-  bool empty() {
-    if (locSize == -1) return true;
-    else return false;
-  }
-private:
-  int size;
-  int locSize;
-  T* arr;
+    ~TStack() {
+      delete []arr;
+    }
+    void push_back(T elem) {
+      if (locSize >= size - 1) {
+        T* tmp = arr;
+        arr = new T[++size];
+        for (int i = 0; i < size; i++) arr[i] = tmp[i];
+        delete[] tmp;
+      }
+      arr[++locSize] = elem;
+    }
+    T& pop_back() {
+      size--;
+      return arr[--locSize + 1];
+    }
+    T& getLast() {
+      return arr[locSize];
+    }
+    bool empty() {
+      if (locSize == -1) return true;
+      else 
+        return false;
+    }
+    
+  private:
+    int size;
+    int locSize;
+    T* arr;
 };
 #endif  // INCLUDE_TSTACK_H_
